4e24458a8a58bc9be83f50c9907f22e0e7c4016f
belong <coder-belong@foxmail.com>
2024-09-24 15:13:15 +0800

    fix: 更新代码

--- pages/04_基于socket.io完成完善聊天室/app.js
+++ pages/04_基于socket.io完成完善聊天室/app.js
@@ -11,18 +11,28 @@ const io = new Server(4003, {
   },
 });
 
+// 用于存储每个连接的客户端信息，其中键是唯一标识 socket.id，值是客户端的用户名，这样做的目的是为了将连接的客户端和其对应的用户名关联起来。
+// 数据格式 users: { FOafKcvDm3K9jfieAAAB: '11', CW42xRoGv78keuXRAAAD: '22' }
 const users = {};
 
 io.on("connection", (socket) => {
-  // 监听客户端的join事件
+  // socket：是一个对象，它代表了与客户端的连接。当客户端连接到服务器时，服务器会为每个连接创建一个 socket 对象
+  // socket.id 是一个唯一标识符，它是由服务器为每个连入的客户端生成的。这个标识符在整个通信过程中保持不变，用于区分不同的客户端连接。
+
+  // 监听客户端的 join 事件
   socket.on("join", (username) => {
+    // 将用户名存储在 users 对象中，键为 socket.id，值为用户名
     users[socket.id] = username;
+    console.log("users:", users);
+    // 发送 userJoined 事件给所有客户端，并携带用户名信息
     io.emit("userJoined", username);
   });
 
-  socket.on("message", (message) => {
-    io.emit("message", {
-      user: users[socket.id],
+  // 监听客户端发送的 chatMessage 事件
+  socket.on("chatMessage", (message) => {
+    // 携带响应数据数据发送 chatMessage 事件给所有客户端
+    io.emit("chatMessage", {
+      userName: users[socket.id],
       text: message,
       time: new Date().toLocaleTimeString(),
     });
